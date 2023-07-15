8067f663fe6bb99f0dd6aa79187ae3e1752bb7fe
Auzdora <1076388287@qq.com>
2023-07-15 13:07:15 +0800

    feat(p3): sequence scan executor done.

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -14,10 +14,32 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx), plan_(plan) {}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() {}
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  table_oid_t toid = plan_->GetTableOid();
+  TableInfo* info = exec_ctx_->GetCatalog()->GetTable(toid);
+
+  if (!current_position_) {
+    // This is the first call to Next, initialize the iterator
+    current_position_ = std::make_unique<TableIterator>(info->table_->Begin(exec_ctx_->GetTransaction()));
+  }
+
+  if (*current_position_ == info->table_->End()) {
+    // We have reached the end of the table
+    return false;
+  }
+
+  // Get the current tuple and RID
+  *tuple = **current_position_;
+  *rid = tuple->GetRid();
+
+  // Advance to the next position in the table
+  ++(*current_position_);
+
+  return true;
+}
 
 }  // namespace bustub
--- src/include/execution/executors/seq_scan_executor.h
+++ src/include/execution/executors/seq_scan_executor.h
@@ -50,5 +50,6 @@ class SeqScanExecutor : public AbstractExecutor {
  private:
   /** The sequential scan plan node to be executed */
   const SeqScanPlanNode *plan_;
+  std::unique_ptr<TableIterator> current_position_ = nullptr;
 };
 }  // namespace bustub
