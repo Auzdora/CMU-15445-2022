7f16db4d7c95141bf3b8a93ff5eebf225c03d96e
Auzdora <1076388287@qq.com>
2023-07-30 22:15:53 +0800

    feat(p3): limit executor done.

--- src/include/execution/executors/limit_executor.h
+++ src/include/execution/executors/limit_executor.h
@@ -12,6 +12,7 @@
 
 #pragma once
 
+#include <cstddef>
 #include <memory>
 #include <utility>
 
@@ -53,5 +54,7 @@ class LimitExecutor : public AbstractExecutor {
   const LimitPlanNode *plan_;
   /** The child executor from which tuples are obtained */
   std::unique_ptr<AbstractExecutor> child_executor_;
+  /** The current generate size*/
+  size_t curr_size_{0};
 };
 }  // namespace bustub
